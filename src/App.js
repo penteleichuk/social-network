f62d942a44b591cc71860d8007ee00ed3567864c
vasyok28 <waitoatime@gmail.com>
2021-12-10 20:16:47 +0200

    11 - grird

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import Header from "./Header";
-import Technologies from "./Technologies";
-import Footer from "./Footer";
+import "./App.css"
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
-            <Footer/>
+        <div className="app-wrapper">
+            <header className="header">
+                <img src="https://brainrain.com.ua/wp-content/uploads/2016/11/intel-company-logo-png-hd-sk.png"
+                     width={100} height={100} alt=""/>
+            </header>
+            <nav className="nav">
+                <div><a href="#1">Profile</a></div>
+                <div><a href="#1">Messages</a></div>
+                <div><a href="#1">News</a></div>
+                <div><a href="#1">Music</a></div>
+                <div><a href="#1">Settings</a></div>
+            </nav>
+            <div className="content">
+                <div>
+                    <img src="https://www.ilmubahasainggris.com/wp-content/uploads/2017/03/NGC.jpg" alt=""/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>New post</div>
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
         </div>
     );
 }
