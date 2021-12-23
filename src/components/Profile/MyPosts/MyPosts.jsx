a4851e36142fb809e40d0c711afea5233364c53a
vasyok28 <waitoatime@gmail.com>
2021-12-24 00:07:30 +0200

    lesson 31

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,16 +1,26 @@
 import style from './MyPosts.module.css'
 import Post from "./Post/Post";
+import {useRef} from "react";
 
 const MyPosts = ({postsData, ...props}) => {
+
+    const postsElements = postsData.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
+
+    const newPostElement = useRef(null);
+
+    const addPost = () => {
+        alert(newPostElement.current.value);
+    }
+
     return (
         <div className={style.postsBlock}>
             My posts
             <div>
-                <textarea></textarea>
-                <button>Add post</button>
+                <textarea ref={newPostElement}></textarea>
+                <button onClick={addPost}>Add post</button>
             </div>
             <div className={style.posts}>
-                {postsData.map(p => <Post message={p.message} likesCount={p.likesCount}/>)}
+                {postsElements}
             </div>
         </div>
     )
