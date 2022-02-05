8af16867c5eacdd01aaaec3889156d7e9ffb9b5b
vasyok28 <waitoatime@gmail.com>
2022-02-05 21:21:52 +0200

    fix route in github

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import {App} from './App';
 import './index.css';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <App/>
         </Provider>
