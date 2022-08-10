24e89194f646982dd91c3cf91d3d094bfe389576
vasyok28 <waitoatime@gmail.com>
2022-08-10 13:43:45 +0300

    fix pagination

--- src/components/Common/Pagination/Pagination.tsx
+++ src/components/Common/Pagination/Pagination.tsx
@@ -1,19 +1,47 @@
 import styles from "./Pagination.module.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 type PaginationPropsType = {
-    pages: Array<number>
+    totalItemsCount: number
+    pageSize: number
     currentPage: number
+    partionSize?: number
     onPageChanged: (pageNumber: number) => void
 }
 
-export const Pagination = (props: PaginationPropsType) => {
+export const Pagination = ({ totalItemsCount, pageSize, currentPage, onPageChanged, partionSize = 10 }: PaginationPropsType) => {
+    const pagesCount = Math.ceil(totalItemsCount / pageSize);
+    const pages = [];
+    for (let i = 1; i <= totalItemsCount; i++) {
+        pages.push(i);
+    }
+
+    const partionCount = Math.ceil(pagesCount / partionSize);
+    const [partionNumber, setPartionNumber] = useState(1);
+    const leftPartionPageNumber = (partionNumber - 1) * partionSize + 1;
+    const rightPartionPageNumber = partionNumber * partionSize;
+
+    const setPartionNumberPrevHandler = () => {
+        setPartionNumber(partionNumber - 1);
+    }
+
+    const setPartionNumberNextHandler = () => {
+        setPartionNumber(partionNumber + 1);
+    }
+
+    useEffect(() => {
+        setPartionNumber(Math.ceil(currentPage / partionSize))
+    }, [partionSize, currentPage]);
+
     return (
         <div className={styles.pagination}>
-            {props.pages.map((p, key) => {
-                return <span key={key} className={(props.currentPage === p ? styles.paginationActive : '')}
-                             onClick={() => {props.onPageChanged(p)}}>{p}</span>
-            })}
+            {partionNumber > 1 && <span onClick={setPartionNumberPrevHandler} >Prev</span>}
+            {pages.filter(p => p >= leftPartionPageNumber && p <= rightPartionPageNumber)
+                .map((p, key) => {
+                    return <span key={key} className={(currentPage === p ? styles.paginationActive : '')}
+                        onClick={() => { onPageChanged(p) }}>{p}</span>
+                })}
+            {partionCount >= partionNumber && <span onClick={setPartionNumberNextHandler} >Next</span>}
         </div>
     );
 }
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,12 +1,11 @@
-import React from "react";
 import styles from "./Users.module.css";
 import userPhoto from "../../assets/images/user.png";
-import {UserType} from "./UsersContainer";
-import {Pagination} from "../Common/Pagination/Pagination";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faUserCog} from "@fortawesome/free-solid-svg-icons";
-import {NavLink} from "react-router-dom";
-import {IconProp} from "@fortawesome/fontawesome-svg-core";
+import { UserType } from "./UsersContainer";
+import { Pagination } from "../Common/Pagination/Pagination";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCog } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 type UsersPPropsType = {
     totalUsersCount: number
@@ -20,46 +19,41 @@ type UsersPPropsType = {
 }
 
 export const Users = (props: UsersPPropsType) => {
-    const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
-
-    const pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i);
-    }
-
     return (
         <div className={styles.users}>
             <div className={styles.usersTitle}>
-                <FontAwesomeIcon icon={faUserCog as IconProp}/>
+                <FontAwesomeIcon icon={faUserCog as IconProp} />
                 Users list
             </div>
             <div className={styles.usersList}>
-                {
-                    props.users.map(u =>
-                        <div key={u.id} className={styles.usersItem}>
-                            <div className={styles.userPhoto}>
-                                <NavLink to={`/profile/${u.id}`}>
-                                    <img src={u.photos.small || userPhoto} alt=""/>
-                                </NavLink>
-                            </div>
-                            <div className={styles.usersInfo}>
-                                <div className={styles.usersName}>{u.name}</div>
-                                {/*<div className={styles.usersStatus}>{u.status}</div>*/}
-                            </div>
-                            <div className={styles.usersButtons}>
-                                {
-                                    u.followed
-                                        ? <button disabled={props.followingInProgress.some(id => id === u.id)}
-                                                  onClick={() => props.unFollow(u.id)}>UnFollow</button>
-                                        : <button disabled={props.followingInProgress.some(id => id === u.id)}
-                                                  onClick={() => props.follow(u.id)}>Follow</button>
-                                }
-                            </div>
+                {props.users.map(u =>
+                    <div key={u.id} className={styles.usersItem}>
+                        <div className={styles.userPhoto}>
+                            <NavLink to={`/profile/${u.id}`}>
+                                <img src={u.photos.small || userPhoto} alt="" />
+                            </NavLink>
                         </div>
-                    )
-                }
+                        <div className={styles.usersInfo}>
+                            <div className={styles.usersName}>{u.name}</div>
+                            {/*<div className={styles.usersStatus}>{u.status}</div>*/}
+                        </div>
+                        <div className={styles.usersButtons}>
+                            {
+                                u.followed
+                                    ? <button disabled={props.followingInProgress.some(id => id === u.id)}
+                                        onClick={() => props.unFollow(u.id)}>UnFollow</button>
+                                    : <button disabled={props.followingInProgress.some(id => id === u.id)}
+                                        onClick={() => props.follow(u.id)}>Follow</button>
+                            }
+                        </div>
+                    </div>
+                )}
             </div>
-            <Pagination pages={pages} currentPage={props.currentPage} onPageChanged={props.onPageChanged}/>
+            <Pagination
+                totalItemsCount={props.totalUsersCount}
+                pageSize={props.pageSize}
+                currentPage={props.currentPage}
+                onPageChanged={props.onPageChanged} />
         </div>
     )
 }
